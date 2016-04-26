efb29c21649505e9de859cd84226eb5e037b6f34
Luke Shirnia <lukeshirnia@btinternet.com>
2016-04-26 20:46:52 +0100

    Update and rename .gitignore to score.sh

---
+++ score.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in $(find /proc -maxdepth 1 -type d); do
+
+if [ -f "$i"/oom_score ]; then
+  printf "$(cat "$i"/oom_score) ";
+  pid=$(echo "$i" | awk -F'/' '{print $3}';)
+    if [ "$(cat "$i"/oom_score)" -ge 1 ];then
+  printf  "$(ps -p "$pid" -o comm=) ";
+    fi
+  printf "$i/oom_score \n";
+fi;
+
+done | awk '$1 > 1' | sort -n -k1
